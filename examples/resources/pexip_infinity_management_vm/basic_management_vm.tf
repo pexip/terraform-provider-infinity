91ef417a8bdcaae89631b61ae096716344889f58
Mike Assel <mike.assel@pexip.com>
2026-04-22 17:14:11 -0400

    update management_vm docs

--- examples/resources/pexip_infinity_management_vm/basic_management_vm.tf
+++ examples/resources/pexip_infinity_management_vm/basic_management_vm.tf
@@ -1,6 +1,13 @@
 resource "pexip_infinity_management_vm" "example" {
-  name        = "Primary Management VM"
-  description = "Primary Pexip Infinity Manager"
+  name        = "Example mgr"
   dns_servers = [pexip_infinity_dns_server.example.id]
   ntp_servers = [pexip_infinity_ntp_server.example.id]
+}
+
+resource "pexip_infinity_dns_server" "example" {
+  address = "8.8.8.8"
+}
+
+resource "pexip_infinity_ntp_server" "example" {
+  address = "pool.ntp.org"
 }
