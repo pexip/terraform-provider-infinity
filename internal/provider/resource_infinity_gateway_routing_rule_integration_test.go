c4fe132202bea7a8a583da08be0b73fc313eaf9d
Mike Assel <mike.assel@pexip.com>
2026-05-29 10:21:12 -0400

    refactor: remove redundant test function for gateway routing rule integration

--- internal/provider/resource_infinity_gateway_routing_rule_integration_test.go
+++ internal/provider/resource_infinity_gateway_routing_rule_integration_test.go
@@ -42,28 +42,6 @@ func TestInfinityGatewayRoutingRuleIntegration(t *testing.T) {
 	)
 	require.NoError(t, err)
 
-	testInfinityGatewayRoutingRule(t, client)
-}
-
-func TestInfinityGatewayRoutingRuleFullIntegration(t *testing.T) {
-	_ = os.Setenv("TF_ACC", "1")
-
-	client, err := infinity.New(
-		infinity.WithBaseURL(test.INFINITY_BASE_URL),
-		infinity.WithBasicAuth(test.INFINITY_USERNAME, test.INFINITY_PASSWORD),
-		infinity.WithMaxRetries(2),
-		infinity.WithTransport(&http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true, // We need this because default certificate is not trusted
-				MinVersion:         tls.VersionTLS12,
-			},
-			MaxIdleConns:        30,
-			MaxIdleConnsPerHost: 5,
-			IdleConnTimeout:     60 * time.Second,
-		}),
-	)
-	require.NoError(t, err)
-
 	testInfinityGatewayRoutingRuleFullIntegration(t, client)
 }
 
