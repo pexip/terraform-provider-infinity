0e5f45711ff766e95e797510b2f67749fca0a5a2
Mike Assel <mike.assel@pexip.com>
2026-07-10 09:56:37 -0400

    Add missing import for resource in MS Exchange connector

--- internal/provider/resource_infinity_ms_exchange_connector.go
+++ internal/provider/resource_infinity_ms_exchange_connector.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int32planmodifier"
